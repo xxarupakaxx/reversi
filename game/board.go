8a5235e0ac47ee22af707dc92130844c46ec9c5b
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-12-19 00:38:56 +0900

    GameLogicの完成

--- game/board.go
+++ game/board.go
@@ -1,5 +1,7 @@
 package game
 
+import "fmt"
+
 type Board struct {
 	Cells [][]Color
 }
@@ -29,4 +31,137 @@ func NewBoard() *Board {
 	b.Cells[4][5] = Blank
 
 	return b
+}
+
+func (b *Board) PutStone(x, y int, c Color) error {
+	if !b.CanPutStone(x, y, c) {
+		return fmt.Errorf("failed to put stone x=%v, y=%v color=%v", x, y, ColorToStr(c))
+	}
+
+	b.Cells[x][y] = c
+
+	for i := -1; i < 1; i++ {
+		for j := -1; j < 1; j++ {
+			if i == 0 && j == 0 {
+				continue
+			}
+			if b.CountTurnableStonesByDirection(x, y, i, j, c) > 0 {
+				b.TurnStonesByDirection(x, y, i, j, c)
+			}
+		}
+	}
+
+	return nil
+}
+
+// CanPutStone 石を置けるかどうかの判定
+func (b *Board) CanPutStone(x, y int, c Color) bool {
+	if b.Cells[x][y] != Empty {
+		return false
+	}
+
+	for i := -1; i <= 1; i++ {
+		for j := -1; j <= 1; j++ {
+			if i == 0 && j == 0 {
+				continue
+			}
+
+			if b.CountTurnableStonesByDirection(x, y, i, j, c) > 0 {
+				return true
+			}
+
+		}
+	}
+	return false
+}
+
+// CountTurnableStonesByDirection ある方向にひっくり返すことのできる石がいくつあるか
+func (b *Board) CountTurnableStonesByDirection(x, y, dx, dy int, c Color) int {
+	cnt := 0
+
+	nx := x + dx
+	ny := y + dy
+	for {
+		nc := b.Cells[nx][ny]
+
+		if nc != OpponentColor(c) {
+			break
+		}
+
+		cnt++
+
+		nx += dx
+		ny += dy
+	}
+
+	if cnt > 0 && b.Cells[nx][ny] == c {
+		return cnt
+	}
+
+	return 0
+}
+
+// TurnStonesByDirection  石をひっくり返す
+func (b *Board) TurnStonesByDirection(x, y, dx, dy int, c Color) {
+	nx := x + dx
+	ny := y + dy
+
+	for true {
+		nc := b.Cells[nx][ny]
+
+		if nc != OpponentColor(c) {
+			break
+		}
+
+		b.Cells[nx][ny] = c
+
+		nx += dx
+		ny += dy
+	}
+}
+
+// AvailableCellCount 盤面の置けるマスの数
+func (b *Board) AvailableCellCount(c Color) int {
+	cnt := 0
+
+	for i := 1; i < 9; i++ {
+		for j := 1; j < 9; j++ {
+			if b.CanPutStone(i, j, c) {
+				cnt++
+			}
+		}
+	}
+
+	return cnt
+}
+
+// Score 盤面の石の数を数える
+func (b *Board) Score(c Color) int {
+	cnt := 0
+
+	for i := 1; i < 9; i++ {
+		for j := 1; j < 9; j++ {
+			if b.Cells[i][j] != c {
+				continue
+			}
+			cnt++
+		}
+	}
+
+	return cnt
+}
+
+// Rest 盤面の置けるマスの数を数える
+func (b *Board) Rest() int {
+	cnt := 0
+
+	for i := 1; i < 9; i++ {
+		for j := 1; j < 9; j++ {
+			if b.Cells[i][j] == Empty {
+				cnt++
+			}
+		}
+	}
+
+	return cnt
 }
